d52d746588757127e5800124b1f95e64b48eb1b3
Nguyễn Anh Quân <130782632+anhquan-ngg@users.noreply.github.com>
2024-10-30 01:57:44 +0700

    Update constants.js

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,5 @@
-export const HOST = import.meta.env.VITE_SERVER_URL;
+export const HOST = "deploy-chat-app-beta.vercel.app"
+;
 
 export const AUTH_ROUTES = "api/auth";
 export const SIGNUP_ROUTE = `${AUTH_ROUTES}/signup`;
