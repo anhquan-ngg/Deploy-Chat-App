87557f72a77f76aaf9e723cc9b3e99c2f1e51e4e
Nguyễn Anh Quân <130782632+anhquan-ngg@users.noreply.github.com>
2024-10-30 01:44:47 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const databaseURL = process.env.DATABASE_URL;
 
 app.use(
     cors({
-        origin: process.env.ORIGIN,
+        origin: "https://deploy-react-chat-app.vercel.app",
         methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
         credentials: true,
     })
