3b7ad04140ef4af4f3d110b077f3dbbe0baf718c
Nguyễn Anh Quân <130782632+anhquan-ngg@users.noreply.github.com>
2024-10-30 09:45:23 +0700

    Update socket.js

--- server/socket.js
+++ server/socket.js
@@ -5,7 +5,7 @@ import Channel from './models/ChannelModel.js';
 const setupSocket = (server) => {
     const io = new SocketIOServer(server, {
         cors: {
-            origin: process.env.ORIGIN,
+            origin: "https://deploy-chat-app-front-end-psi.vercel.app",
             methods: ["GET", "POST"],
             credentials: true,
         }
